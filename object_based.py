6d9df70e97a5447b9162f23a78ac558fea075967
mooshi_1 <achet92@gmail.com>
2024-12-17 19:34:25 -0500

    made a wizard duel

--- object_based.py
+++ object_based.py
@@ -1,2 +1,56 @@
 #starting notes to better solidify knowledge of object based programming in python
 
+#create wizard class and simulate magic duels
+def main():
+    gandalf = Wizard("Gandalf", 30, 50)
+    saruman = Wizard("Saruman", 20, 40) 
+
+    def print_status():
+        print(f"\n{gandalf.name}: Health={gandalf.health}, Mana={gandalf.mana}")
+        print(f"{saruman.name}: Health={saruman.health}, Mana={saruman.mana}\n")
+    
+    print("The wizard duel begins!")
+    print_status()
+
+    try:
+        rounds = 2
+        for i in range(1, rounds+1):
+            if i % 2 == 0:
+                gandalf.cast_spell(saruman)
+                saruman.cast_spell(gandalf)
+                print(f"Round {i} of {rounds} complete.")
+                print_status()
+            if i % 2 != 0:
+                saruman.cast_spell(gandalf)
+                gandalf.cast_spell(saruman)
+                print(f"Round {i} of {rounds} complete.")
+                print_status()
+
+        # Now what would you add here to make them fight?
+        # Remember to use print_status() after each spell!
+        
+    except Exception as e:
+        print(e)
+
+class Wizard:
+    def __init__(self, name, mana, health):
+        self.name = name
+        self.mana = mana
+        self.health = health
+    
+    def cast_spell(self, target):
+        if self.mana < 10:
+            raise Exception(f"{self.name} is out of mana")
+        self.mana -=10
+        print(f"{self.name} casts fireball on {target.name}")
+        target.take_damage()
+
+    def take_damage(self):
+        if self.health > 0:
+            self.health -= 20
+        if self.health <= 0:
+            raise Exception(f"{self.name} has been defeated.")
+
+
+if __name__ == "__main__":
+    main()
