6c868c9549181231175956cab9d3b0408fb6abc5
mooshi_1 <achet92@gmail.com>
2024-12-18 00:55:48 -0500

    library lesson, dealing with multiple objects

--- object_based.py
+++ object_based.py
@@ -126,3 +126,105 @@ main()
 
 # fire dragon does 300 damage
 # ice dragon does 150 damage
+
+
+class Employee:
+    
+    company_name = "Age of Dragons, Inc."
+    total_employees = 0
+    
+    def __init__(self, first_name, last_name, id, position, salary):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.id = id
+        self.position = position
+        self.salary = salary
+        Employee.total_employees += 1
+
+    def get_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+# look at the line employee.total_employees -- the class of employee was operated on inside __init__
+# In Python, there are two main types of variables in a class:
+
+#     Instance variables (using self.) which are unique to each object
+#     Class variables (using ClassName.) which are shared across ALL objects of that class
+
+
+
+# self.first_name is used inside the class methods to refer to the current instance's attribute
+# employee.first_name is used outside the class when you have a specific instance stored in a variable named employee
+
+# Here's a simple example to illustrate:
+
+class Employee:
+    def __init__(self, first_name):
+        self.first_name = first_name  # 'self' refers to current instance being created
+        
+    def print_name(self):
+        print(self.first_name)  # 'self' refers to whatever instance called this method
+
+# Outside the class
+employee1 = Employee("John")
+employee1.first_name  # same value as 'self.first_name' was inside the class
+employee1.print_name()  # when this runs, 'self' becomes 'employee1'
+
+# Think of self as a placeholder that automatically becomes whatever instance is being used. If I create another employee:
+
+# employee2 = Employee("Jane")
+
+# When methods run on employee2, self refers to employee2 instead of employee1.
+
+# Does this help explain the relationship between self and instance variables? Would you like to see another example?
+
+#----------------------------------------------------------
+#library challenge
+#have 2 objects, books and librarys
+
+
+class Book:
+    #define the Book class and give it the attributes
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+    #allows me to print the Book object as I use it in the library
+    def __str__(self):
+        return f"{self.title} by {self.author}"
+        
+class Library:
+
+    #initialize library for user name
+    def __init__(self, name):
+        self.name = name
+        self.all_books = []
+
+    #passing Book object as book. list is appended with an object
+    def add_book(self, book):
+        self.all_books.append(book)
+
+    #delete from book list
+    def remove_book(self, book):
+        for i in self.all_books:
+            #i is the Book object - contains title and author information
+            #compare Book object and Library object
+            #print (i)
+            if i.title == book.title and i.author == book.author:
+                self.all_books.remove(i)    
+            
+    #take a string
+    #compare to author and title
+    #return matches from library
+    def search_books(self, search_string):
+        #create empty list to append matches
+        search_matches = []
+        
+        #self.all_books is assigned to our current object and can be iterated to produce 
+        #Book objects from the list of books
+        
+        for book in self.all_books:
+            if (search_string.lower() in book.author.lower() or 
+            search_string.lower() in book.title.lower()):
+                search_matches.append(book)
+        return search_matches
