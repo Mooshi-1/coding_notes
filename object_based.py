9620a98f9fbeb061d6c4967af420897101c16f07
mooshi_1 <achet92@gmail.com>
2024-12-18 22:35:31 -0500

    dragon fight

--- object_based.py
+++ object_based.py
@@ -328,3 +328,69 @@ class House(Residential):
     def __init__(self, location, bedrooms, yard_size):
         super().__init__(location, bedrooms)
         self.__yard_size = yard_size
+
+    
+    #calling on functions set up for a dragon fight
+
+    def main():
+    dragons = [
+        Dragon("Green Dragon", 0, 0, 1),
+        Dragon("Red Dragon", 2, 2, 2),
+        Dragon("Blue Dragon", 4, 3, 3),
+        Dragon("Black Dragon", 5, -1, 4),
+    ]
+
+
+    for dragon in dragons:
+        describe(dragon)
+
+    #each dragon breathe fire at 3,3 - pass in all other~~ dragons as the unit
+    for obj in dragons:    
+        dragons_copy = dragons.copy()
+        dragons_copy.remove(obj)
+        obj.breathe_fire(3,3,dragons_copy)
+        
+
+
+def describe(dragon):
+    print(f"{dragon.name} is at {dragon.pos_x}/{dragon.pos_y}")
+
+
+class Unit:
+    def __init__(self, name, pos_x, pos_y):
+        self.name = name
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+
+    def in_area(self, x_1, y_1, x_2, y_2):
+        return (
+            self.pos_x >= x_1
+            and self.pos_x <= x_2
+            and self.pos_y >= y_1
+            and self.pos_y <= y_2
+        )
+
+
+class Dragon(Unit):
+    def __init__(self, name, pos_x, pos_y, fire_range):
+        super().__init__(name, pos_x, pos_y)
+        self.__fire_range = fire_range
+
+    def breathe_fire(self, x, y, units):
+        print(f"{self.name} breathes fire at {x}/{y} with range {self.__fire_range}")
+        print("====================================")
+        for unit in units:
+            in_area = unit.in_area(
+                x - self.__fire_range,
+                y - self.__fire_range,
+                x + self.__fire_range,
+                y + self.__fire_range,
+            )
+            if in_area:
+                print(f"{unit.name} is hit by the fire")
+
+
+main()
+
+
+##
